4bce52cc703ff7ff71e9b3e8e08c97531c9efd85
S-M-Usman <shaikhmuhammadusmanofficial@gmail.com>
2025-01-29 19:11:41 +0500

    app

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    SANITY_ACCESS_TOKEN: process.env.SANITY_ACCESS_TOKEN,
+  },
   images: {
     remotePatterns: [
       {
